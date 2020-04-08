3b37c152b169bb948be9364eb67d67bece3bfb7e
Olav Ljosland <olavlj@me.com>
2020-04-08 18:43:14 +0200

    Add swagsquad room

--- react/rooms.web.js
+++ react/rooms.web.js
@@ -27,6 +27,43 @@ const data = {
             mainRoom: false,
             color: "#e74c3c"
         }
+    ],
+    swagsquad: [
+        {
+            name: "Stua",
+            mainRoom: true,
+            color: "#34495e"
+        },
+        {
+            name: "Olav sitt rom",
+            mainRoom: false,
+            color: "#2ecc71"
+        },
+        {
+            name: "Lars sitt rom",
+            mainRoom: false,
+            color: "#2980b9"
+        },
+        {
+            name: "Andreas sitt rom",
+            mainRoom: false,
+            color: "#2980b9"
+        },
+        {
+            name: "Trude sitt rom",
+            mainRoom: false,
+            color: "#e74c3c"
+        },
+        {
+            name: "Espen sitt rom",
+            mainRoom: false,
+            color: "#d35400"
+        },
+        {
+            name: "Marianne sitt rom",
+            mainRoom: false,
+            color: "#f1c40f"
+        }
     ]
 };
 
